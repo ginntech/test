937684eb83c10be3d481794a379600354adc926e
ginntech <ginntech@163.com>
2022-03-25 16:14:11 +0800

    修改

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
-    echo "hello world!</br>";
-    echo "hello ginn";
-    echo "\(^o^)/~";
-    echo "v1.0";
+    class Test_Class{
+        public $name = null;
+        public $age = null;
+        public $job = null;
+        
+        function __construct($name,$age,$job){
+            $this->name = $name;
+            $this->age = $age;
+            $this->job = $job;
+            echo "Name is:".$name."\n";
+            echo "Age is:".$age."\n";
+            echo "Job is:".$job."\n";
+        }
+
+        function output(){
+            echo "222\n";
+        }
+    }
+    
+    $obj = new Test_Class("Frank8","38","IT");
+    //echo $obj->name."\n";
+    //$obj->output();
